dafb6845546b32168a301253ed1999a6c65cc534
kimura-bit <82050227+kimura-bit@users.noreply.github.com>
2022-08-17 14:25:09 +0100

    new commit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Hello Rhys';
+  const name = 'Hello Rhys. This is a test';
   greetUser(name);
 });
